e6bf484a5ab27311c6b5e041316ba66f646700d9
mariusrueve <marius.rueve@live.de>
2023-12-05 10:14:27 +0100

    solve day1 1/2

---
+++ day1/trebuchet.py
@@ -0,0 +1,27 @@
+def main():
+    # read file line by line and print the line
+    calibration_values = []
+
+    with open('1/puzzle_input.txt', 'r') as f:
+        for line in f:
+            first_digit = None
+            last_digit = None
+            for char in line:
+                # if char is a digit
+                if char.isdigit():
+                    if first_digit is None:
+                        first_digit = char
+                    else:
+                        last_digit = char
+            if first_digit is not None and last_digit is None:
+                calibration_values.append(int(first_digit + first_digit))
+            elif first_digit is not None and last_digit is not None:
+                calibration_values.append(int(first_digit + last_digit))
+            else:
+                continue
+    # print sum of all calibration values
+    print(sum(calibration_values))
+
+
+if __name__ == '__main__':
+    main()
